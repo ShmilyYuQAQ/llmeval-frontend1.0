964b060d623c8d2e82e2f3cf6ccc120893fbb116
ylovecha <tianelove0819@163.com>
2025-11-20 17:49:32 +0800

    試一下限制不用HTTP2

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,4 +3,7 @@ const axiosInstance = axios.create({
     baseURL: "http://49.233.82.133:9091",
     timeout: 50000,
 });
+
+// 直接改实例自己的默认 header（一定生效！）
+axiosInstance.defaults.headers.common['X-HTTP2-Support'] = 'no';
 export default axiosInstance;
